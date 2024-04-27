c7c336f7eefa937f9dced0d744acbcb9e3c470e3
YuriiOdynets <164253487+YuriiOdynets@users.noreply.github.com>
2024-04-27 22:00:44 +0200

    Delete data.py

--- data.py
+++
@@ -1,5 +0,0 @@
-#Function for user data input. Removes extra spaces and changes input to lower register
-def parse_input(user_input):
-    cmd, *args = user_input.split()
-    cmd = cmd.strip().lower()
-    return cmd, *args
